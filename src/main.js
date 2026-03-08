b6a509511421e68bff60a411bee1b7b7ae79c8f2
Prasetyo2122 <m.arifandiprasetyo354>
2026-03-08 23:51:02 +0700

    fixx

--- src/main.js
+++ src/main.js
@@ -1,21 +1,18 @@
-// 1. Import CSS Utama (Sangat krusial agar Tailwind v4 berjalan)
+
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia' // Untuk mengelola data antar fitur
+import { createPinia } from 'pinia' 
 import App from './App.vue'
-import router from './router' // Memanggil konfigurasi rute yang baru kita buat
+import router from './router' 
 
 const app = createApp(App)
 
-// 2. Gunakan Pinia (State Management)
-// Karena kamu backend dev, anggap Pinia ini sebagai "database lokal" (store)
-// untuk menyimpan catatan revisi atau status tugas Kanban.
+
 app.use(createPinia())
 
-// 3. Gunakan Router
-// Agar navigasi maksimal 15 halaman berjalan tanpa reload
+
 app.use(router)
 
-// 4. Mount (Tampilkan) ke dalam file index.html
+
 app.mount('#app')
